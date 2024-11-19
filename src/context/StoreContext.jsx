4c8b92399b4ffe089d4458fe018e22809502aa8a
ephrem t <tilahunephrem03@gmail.com>
2024-11-19 08:25:32 -0800

    add footer and app-download

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,18 +1,39 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
+  const[cartItems,setCartItems] = useState({});
+
+  const addToCart = (itemID) => {
+    if (!cartItems[itemID]){
+      setCartItems((prev)=>({...prev,[itemID]:1}))
+    }
+    else{
+      setCartItems((prev)=>({...prev,[itemID]:prev[itemID]+1}))
+    }
+  }
+
+  const removeFromCart = (itemID) => {
+    setCartItems((prev)=>({...prev,[itemID]:prev[itemID]-1}))
+  }
 
 
-  
+  useEffect(()=>{
+    console.log(cartItems);
+
+  },[cartItems])
   
 
 
 
   const contextValue = {
-    food_list
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
 
   }
   return (
